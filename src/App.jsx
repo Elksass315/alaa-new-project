b01435cf62cb8f05b9d70594da4b27fc5b5cf73c
Elksass315 <qsasomar@gmail.com>
2025-05-06 02:22:30 +0300

    Refactor code structure for improved readability and maintainability

--- src/App.jsx
+++ src/App.jsx
@@ -8,68 +8,98 @@ import Nav from "./nav";
 import Team from "./team";
 import Contact from "./contact";
 import Flay from "./flay";
+import { PhoneIcon, PlayCircleIcon, SparklesIcon } from '@heroicons/react/20/solid';
+
+
+const DrinksSolutions = [
+  { name: 'Starlight Mojito', description: 'Get a better understanding of your traffic', href: '#', icon: SparklesIcon ,price: 60},
+  { name: 'Galaxy Smoothie', description: 'Speak directly to your customers', href: '#', icon: SparklesIcon, price:65 },
+  { name: 'Milky Way Shake', description: "Your customers' data will be safe and secure", href: '#', icon: SparklesIcon, price: 80 },
+  { name: 'Zero Gravity Coffee', description: 'Connect with third-party tools', href: '#', icon: SparklesIcon, price: 100 },
+  { name: 'Cosmic Blue Lemonade', description: 'Build strategic funnels that will convert', href: '#', icon: SparklesIcon, price: 110 },
+];
+
+const dessertsSolutions = [
+  { name: 'stardust Donuts', description: 'Get a better understanding of your traffic', href: '#', icon: SparklesIcon, price: 50 },
+  { name: 'Nebula Ice Cream', description: 'Speak directly to your customers', href: '#', icon: SparklesIcon, price: 30 },
+  { name: 'Planetary CheeseCake', description: "Your customers' data will be safe and secure", href: '#', icon: SparklesIcon, price: 60 },
+  { name: 'Moon Rock Brownies', description: 'Connect with third-party tools', href: '#', icon: SparklesIcon, price: 65 },
+  { name: 'Black Hole Lava Cake', description: 'Build strategic funnels that will convert', href: '#', icon: SparklesIcon, price: 80 },
+];
+const MainDishesSolutions = [
+  { name: 'Galactic Burger', description: 'Get a better understanding of your traffic', href: '#', icon: SparklesIcon, price: 160 },
+  { name: 'Nebula Steak', description: 'Speak directly to your customers', href: '#', icon: SparklesIcon, price: 120 },
+  { name: 'Meteor Fries', description: "Your customers' data will be safe and secure", href: '#', icon: SparklesIcon, price: 140 },
+  { name: 'Cosmic Pasta', description: 'Connect with third-party tools', href: '#', icon: SparklesIcon, price: 130 },
+  { name: 'Supernova Chicken', description: 'Build strategic funnels that will convert', href: '#', icon: SparklesIcon, price: 110 },
+];
+const callsToAction = [
+  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
+  { name: 'Contact sales', href: '#', icon: PhoneIcon },
+];
 
 function App() {
   return (
-   <>
-    <Nav/>
-    <div>
-      {/* Background Image */}
-      <div
-        className="fixed top-0 left-0 w-screen h-screen bg-no-repeat bg-cover bg-center mx-0 -z-10"
-        style={
-          {
-            // backgroundImage: "url('/public/bg.png')",
+    <>
+      {/* <Nav/> */}
+      <div>
+        {/* Background Image */}
+        <div
+          className="fixed top-0 left-0 w-screen h-screen bg-no-repeat bg-cover bg-center mx-0 -z-10"
+          style={
+            {
+              // backgroundImage: "url('/public/bg.png')",
+            }
           }
-        }
-      ></div>
-      {/* Content */}
-      <div className="relative z-500">
-        <div className="h-screen flex flex-col items-center justify-center font-bold text-shadow-lg text-shadow-fuchsia-400 text-3xl  ">
-          {/* Welcome to Rbit Bits B litter shoud be bold and have 5px shadow and the  i need it to have less space around  */}
-          <h1 className="text-white text-4xl bg-blue-950 p-3 border-2 border-blue-900 rounded-lg   ">
-            Welcome to{" "}
-            <span className="text-fuchsia-700 text-shadow-fuchsia-400 text-shadow-lg font-bold -mr-3">
-              🪐
-            </span>
-            rbit
-            <span className="text-fuchsia-700 text-shadow-fuchsia-400 text-shadow-lg font-bold ml-3 ">
-              B
-            </span>
-            ites{" "}
-          </h1>
-          {/* //img to the left take 30 % of the screen and be 100% height */}
-          <img
-            src={img}
-            alt="logo"
-            className=" hidden -z-2 absolute border-fuchsia-700 border-solid  border-4 border-opacity-50 rounded-full  h-1/12 top-3 left-2 "
-          />
+        ></div>
+        {/* Content */}
+        <div className="relative z-500">
+          <div className="h-screen flex flex-col items-center justify-center font-bold text-shadow-lg text-shadow-fuchsia-400 text-3xl  ">
+            {/* Welcome to Rbit Bits B litter shoud be bold and have 5px shadow and the  i need it to have less space around  */}
+            <h1 className="text-white text-4xl bg-blue-950 p-3 border-2 border-blue-900 rounded-lg   ">
+              Welcome to{" "}
+              <span className="text-fuchsia-700 text-shadow-fuchsia-400 text-shadow-lg font-bold -mr-3">
+                🪐
+              </span>
+              rbit
+              <span className="text-fuchsia-700 text-shadow-fuchsia-400 text-shadow-lg font-bold ml-3 ">
+                B
+              </span>
+              ites{" "}
+            </h1>
+            {/* //img to the left take 30 % of the screen and be 100% height */}
+            <img
+              src={img}
+              alt="logo"
+              className=" hidden -z-2 absolute border-fuchsia-700 border-solid  border-4 border-opacity-50 rounded-full  h-1/12 top-3 left-2 "
+            />
 
-          <img
-            src={tC}
-            alt="logo"
-            className="hidden -z-2 absolute border-fuchsia-700 border-solid  border-4 border-opacity-50 rounded-full w-1/5 h-1/12 -top-28  "
-          />
-          <img
-            src={tC}
-            alt="logo"
-            className=" hidden -z-2 absolute border-fuchsia-700 border-solid  border-4 border-opacity-50 rounded-full w-1/5 h-1/12 top-3 right-2  "
-          />
-          <div className="text-white text-shadow-lg text-shadow-fuchsia-400 text-xl block mt-12 ">
-            scroll down
+            <img
+              src={tC}
+              alt="logo"
+              className="hidden -z-2 absolute border-fuchsia-700 border-solid  border-4 border-opacity-50 rounded-full w-1/5 h-1/12 -top-28  "
+            />
+            <img
+              src={tC}
+              alt="logo"
+              className=" hidden -z-2 absolute border-fuchsia-700 border-solid  border-4 border-opacity-50 rounded-full w-1/5 h-1/12 top-3 right-2  "
+            />
+            <div className="text-white text-shadow-lg text-shadow-fuchsia-400 text-xl block mt-12 ">
+              scroll down
+            </div>
+            <div className="text-white text-shadow-lg text-shadow-fuchsia-400 text-3xl block ">
+              ⬇️
+            </div>
           </div>
-          <div className="text-white text-shadow-lg text-shadow-fuchsia-400 text-3xl block ">
-            ⬇️
-          </div>
-        </div>
-        <div className=" flex items-center justify-center flex-col">
-          <Flay/>
-          <Flay/>
-          <Flay/>
-          {/* <h1 className="text-white text-4xl">Sliding Content Section 1</h1> */}
-          {/* <img src={menu} alt="logo" className="w-full max-h-10/12 " /> */}
+          <div className=" flex items-center justify-center flex-col">
+            <Flay name="Drinks" solutions={DrinksSolutions} callsToAction={callsToAction} />
+            <Flay name="Main Dishes" solutions={MainDishesSolutions} callsToAction={callsToAction} />
+            <Flay name="Desserts" solutions={dessertsSolutions} callsToAction={callsToAction} />
 
-          {/* <div className="flex flex-row items-center justify-center">
+            {/* <h1 className="text-white text-4xl">Sliding Content Section 1</h1> */}
+            {/* <img src={menu} alt="logo" className="w-full max-h-10/12 " /> */}
+
+            {/* <div className="flex flex-row items-center justify-center">
           <img src={menu} alt="logo" className="w-1/2 h-1/2" />
             <div className="flex flex-col items-center justify-center">
              <img src={menu} alt="logo" className="w-1/2 h-1/2" />
@@ -81,22 +111,22 @@ function App() {
               TITILE 1
             </div>
           </div> */}
-        </div>
-        <div className="h-screen flex items-center justify-center">
-        <Team/>
-        </div>
-        <div className="">
-         <Contact/>
-        </div>
-        <div className="h-screen flex items-center justify-center">
-          <h1 className="text-white text-4xl">Sliding Content Section 1</h1>
-        </div>
-        <div className="h-screen flex items-center justify-center">
-          <h1 className="text-white text-4xl">Sliding Content Section 2</h1>
+          </div>
+          <div className="h-screen flex items-center justify-center">
+            <Team />
+          </div>
+          <div className="">
+            <Contact />
+          </div>
+          {/* <div className="h-screen flex items-center justify-center">
+            <h1 className="text-white text-4xl">Sliding Content Section 1</h1>
+          </div>
+          <div className="h-screen flex items-center justify-center">
+            <h1 className="text-white text-4xl">Sliding Content Section 2</h1>
+          </div> */}
         </div>
       </div>
-    </div>
-   </>
+    </>
   );
 }
 
--- src/flay.jsx
+++ src/flay.jsx
@@ -1,37 +1,16 @@
+// flay.jsx
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
-import {
-  ArrowPathIcon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
-} from '@heroicons/react/24/outline'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
-const solutions = [
-  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
-  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
-  { name: 'Security', description: "Your customers' data will be safe and secure", href: '#', icon: FingerPrintIcon },
-  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
-  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
-]
-const callsToAction = [
-  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-  { name: 'Contact sales', href: '#', icon: PhoneIcon },
-]
-
-export default function Flay() {
+export default function Flay({name, solutions = [], callsToAction = [] }) {
   return (
     <Popover className="relative">
-      <PopoverButton className="w-2xl inline-flex items-center gap-x-1 text-sm/6 font-semibold text-white  w-2xl m-2.5 p-2  bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg shadow-lg ring-1 ring-gray-900/5 hover:ring-gray-900/10 transition duration-300 ease-in-out">
-        <span>Solutions</span>
+      <PopoverButton className="w-2xl inline-flex items-center gap-x-1 text-sm/6 font-semibold text-white m-2.5 p-2 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg shadow-lg ring-1 ring-gray-900/5 hover:ring-gray-900/10 transition duration-300 ease-in-out">
+        <span>{name}</span>
         <ChevronDownIcon aria-hidden="true" className="size-10" />
       </PopoverButton>
 
-      <PopoverPanel
-        transition
-        className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4 transition data-closed:translate-y-1 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
-      >
+      <PopoverPanel className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
         <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm/6 shadow-lg ring-1 ring-gray-900/5">
           <div className="p-4">
             {solutions.map((item) => (
@@ -39,30 +18,18 @@ export default function Flay() {
                 <div className="mt-1 flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
                   <item.icon aria-hidden="true" className="size-6 text-gray-600 group-hover:text-indigo-600" />
                 </div>
-                <div>
-                  <a href={item.href} className="font-semibold text-gray-900">
+                <div className="flex justify-between w-full items-center gap-2">
+                  <a href={item.href} className="relative z-10 font-semibold text-gray-900 after:content-['..................................................................................'] after:flex-1 after:ml-2 after:overflow-hidden after:whitespace-nowrap after:text-gray-400 after:text-ellipsis after:block">
                     {item.name}
-                    <span className="absolute inset-0" />
                   </a>
-                  <p className="mt-1 text-gray-600">{item.description}</p>
+                  <span className="text-gray-900 whitespace-nowrap">{item.price}</span>
                 </div>
+
               </div>
-            ))}
-          </div>
-          <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-            {callsToAction.map((item) => (
-              <a
-                key={item.name}
-                href={item.href}
-                className="flex items-center justify-center gap-x-2.5 p-3 font-semibold text-gray-900 hover:bg-gray-100"
-              >
-                <item.icon aria-hidden="true" className="size-5 flex-none text-gray-400" />
-                {item.name}
-              </a>
             ))}
           </div>
         </div>
       </PopoverPanel>
     </Popover>
-  )
+  );
 }
